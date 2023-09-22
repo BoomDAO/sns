77f5fad7c74ff041094d87c465f4e560b4b324c5
hitesh <hiteshtripathi12345678@gmail.com>
2023-09-22 21:53:12 +0530

    pushed proposal live

--- proposals/09.2023/motion_follow_beacon_neuron.sh
+++ proposals/09.2023/motion_follow_beacon_neuron.sh
@@ -1,6 +1,3 @@
-export DEVELOPER_NEURON_ID="fa98d03dc1ef9eb4db04f8ce1ced50b7fdd70143e575a507f89775f69f66beac"
-export PEM_FILE="/Users/jarvis/.config/dfx/identity/$(dfx identity whoami)/identity.pem"
-
 quill sns --canister-ids-file ./sns_canister_ids.json --pem-file $PEM_FILE make-proposal  --proposal "(record {
         title = \"Introducing BOOM DAO's Beacon Neuron for Community Consideration (Neuron ID: 91b153596d2484f610bff3d72c42ec0fcb42be319090654959fe2c148eba88d7)\";
         url = \"https://boomdao.xyz/\";
@@ -26,5 +23,5 @@ Numerous members within the BOOM DAO community have expressed keen interest in f
             }
         };
     }
-)" $DEVELOPER_NEURON_ID > extend_voting_period.json
-quill send extend_voting_period.json
+)" $DEVELOPER_NEURON_ID > motion_follow_beacon_neuron.json
+quill send motion_follow_beacon_neuron.json
