965f729f2f0d3354a3e089b0330811c4f5a08f31
hitesh <hiteshtripathi12345678@gmail.com>
2024-02-12 19:38:10 +0530

    all latest proposals

--- proposals/12.2023/upgrade_token_deployer.sh
+++ proposals/12.2023/upgrade_token_deployer.sh
@@ -2,14 +2,15 @@ export PEM_FILE="/Users/jarvis/.config/dfx/identity/default/identity.pem"
 export DEVELOPER_NEURON_ID="fa98d03dc1ef9eb4db04f8ce1ced50b7fdd70143e575a507f89775f69f66beac"
 
 quill sns --canister-ids-file ./sns_canister_ids.json  --pem-file ${PEM_FILE}  make-upgrade-canister-proposal \
-  --title "Upgrade WorldHub canister"  \
-  --summary "This proposal upgrades the WorldHub canister of the World Protocol.
-
+  --title "Upgrade Token Deployer canister"  \
+  --summary "This proposal upgrades the Token Deployer canister. 
+    
 Proposed changes, features or fixes : 
-1. Upgrade WorldHub canister with the latest UserNode class, to support World's refactoring for better handling of actions and configs. " \
+1. Added official ICRC ledger support to Token Deployer.
+2. Added ledger wasms management in Token Deployer to support future upgrades of Token canisters."  \
   --url "https://boomdao.xyz"  \
-  --target-canister-id "j362g-ziaaa-aaaap-abf6a-cai" \
-  --wasm-path "./proposals/12.2023/WorldHub.wasm.gz" \
+  --target-canister-id "jv4xo-cyaaa-aaaap-abf7a-cai" \
+  --wasm-path "./proposals/12.2023/token_deployer.wasm.gz" \
   --canister-upgrade-arg "(record {})" \
     ${DEVELOPER_NEURON_ID} > "message.json"
 
