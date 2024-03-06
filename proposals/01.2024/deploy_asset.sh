e7daab311036a0db1040ed037b687c5be07cc2d4
hitesh <hiteshtripathi12345678@gmail.com>
2024-03-06 15:09:02 +0530

    latest proposals

--- proposals/01.2024/deploy_asset.sh
+++ proposals/01.2024/deploy_asset.sh
@@ -1,6 +1,6 @@
 export PEM_FILE="/Users/jarvis/.config/dfx/identity/default/identity.pem"
 export DEVELOPER_NEURON_ID="fa98d03dc1ef9eb4db04f8ce1ced50b7fdd70143e575a507f89775f69f66beac"
-export BLOB="$(didc encode --format blob "(record { batch_id = 14 : nat; evidence = blob \"\69\c7\39\18\56\72\7f\ba\9b\65\39\b5\f4\e3\a1\92\c5\87\62\ea\86\aa\cc\61\b4\98\7b\ef\a9\28\98\a4\";})")"
+export BLOB="$(didc encode --format blob "(record { batch_id = 17 : nat; evidence = blob \"\4f\96\22\98\80\d8\28\2a\f8\10\e5\84\33\7c\34\58\ad\f6\71\fe\82\3c\93\4d\fc\f8\71\39\6c\f1\05\f1\";})")"
 
 quill sns  --canister-ids-file ./sns_canister_ids.json  --pem-file $PEM_FILE  make-proposal $DEVELOPER_NEURON_ID --proposal "(
     record { 
@@ -8,7 +8,7 @@ quill sns  --canister-ids-file ./sns_canister_ids.json  --pem-file $PEM_FILE  ma
         url = \"https://boomdao.xyz\"; 
         summary = \"This proposal executes generic function commit_proposed_batch on Game Launcher Asset canister. 
 
-Proposed batch includes version bump for packages and fixes to resolve UI bugs in Game Launcher.
+Proposed batch includes UI/UX fixes.
 
 For more information check https://internetcomputer.org/docs/current/developer-docs/integrations/sns/managing/sns-asset-canister#submitting-an-sns-proposal-and-upgrading-an-asset-canister .
 \"; 
--- proposals/09.2023/execute_commit_batch.sh
+++ proposals/09.2023/execute_commit_batch.sh
@@ -1,6 +1,6 @@
 export PEM_FILE="/Users/jarvis/.config/dfx/identity/default/identity.pem"
 export DEVELOPER_NEURON_ID="fa98d03dc1ef9eb4db04f8ce1ced50b7fdd70143e575a507f89775f69f66beac"
-export BLOB="$(didc encode --format blob "(record { batch_id = 12 : nat; evidence = blob \"\11\13\1f\90\e9\ce\c0\da\22\a8\a0\77\af\2c\b7\4a\88\2a\d7\84\9c\27\11\18\83\68\92\67\a7\6c\37\5c\";})")"
+export BLOB="$(didc encode --format blob "(record { batch_id = 15 : nat; evidence = blob \"\9e\03\da\76\93\30\d5\b7\91\4d\dc\13\2f\8f\ca\88\09\44\4d\71\85\c5\c4\26\cf\e5\19\63\d4\cb\8f\df\";})")"
 
 quill sns  --canister-ids-file ./sns_canister_ids.json  --pem-file $PEM_FILE  make-proposal $DEVELOPER_NEURON_ID --proposal "(
     record { 
@@ -8,7 +8,7 @@ quill sns  --canister-ids-file ./sns_canister_ids.json  --pem-file $PEM_FILE  ma
         url = \"https://boomdao.xyz\"; 
         summary = \"This proposal executes generic function commit_proposed_batch on Game Launcher Asset canister. 
 
-Proposed batch includes UX fix for upgrading World canister using Game Launcher frontend feature.
+Proposed batch includes BOOM GAMING GUILD V1.
 
 For more information check https://internetcomputer.org/docs/current/developer-docs/integrations/sns/managing/sns-asset-canister#submitting-an-sns-proposal-and-upgrading-an-asset-canister .
 \"; 
